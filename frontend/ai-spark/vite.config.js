a3ab22f617a8344c34f2877889c9c128036ee5a9
Sangeeta Kundu <sangeetakundu057@gmail.com>
2025-12-22 02:22:14 +0530

    Readme Updated

--- frontend/ai-spark/vite.config.js
+++ frontend/ai-spark/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // increase warning limit
+
+    rollupOptions: {
+      output: {
+        // Automatically split all dependencies from node_modules
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        }
+      }
+    }
+  }
+});
