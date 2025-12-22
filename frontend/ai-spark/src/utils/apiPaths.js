45474a8c864f32826e6ac6a50a07c72fa7d14deb
Sangeeta Kundu <kundusangeeta057@gmail.com>
2025-12-22 11:43:45 +0530

    Update BASE_URL to production endpoint

--- frontend/ai-spark/src/utils/apiPaths.js
+++ frontend/ai-spark/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL="http://localhost:8000";
+export const BASE_URL="https://cv-spark-backend.onrender.com";
 
 //util/apiPaths.js
 export const API_PATHS={
@@ -19,4 +19,5 @@ export const API_PATHS={
     IMAGE:{
         UPLOAD_IMAGE:"/api/auth/upload-image",
     },
+
 };
